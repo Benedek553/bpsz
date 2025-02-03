9ca6f1292886cc0ed1580fa1a6165af676018e60
Farsang Benedek <carnevalben@gmail.com>
2025-02-03 19:00:40 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,14 +1,16 @@
 <?php
-$serverName = "bpsz.database.windows.net";
-$database = "bpsz";
-$username = "sqladmin";
-$password = "Password2024@2024";
-
+// PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server=$serverName;Database=$database", $username, $password);
+    $conn = new PDO("sqlsrv:server = tcp:bpsz.database.windows.net,1433; Database = bpsz", "sqladmin", "Password2024@2024");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (Exception $e) {
-    die("Hiba az adatbáziskapcsolat létrehozásakor: " . $e->getMessage());
 }
-?>
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
 
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "sqladmin", "pwd" => "{your_password_here}", "Database" => "bpsz", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:bpsz.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+?>
