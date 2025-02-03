f728d925821127e2c61252fa12d9928b38c7cd53
Farsang Benedek <carnevalben@gmail.com>
2025-02-03 19:09:42 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-$connectionString = "Driver={ODBC Driver 18 for SQL Server};Server=tcp:bpsz.database.windows.net,1433;Database=bpsz;Uid=sqladmin;Pwd={your_password_here};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;";
+$connectionString = "Driver={ODBC Driver 18 for SQL Server};Server=tcp:bpsz.database.windows.net,1433;Database=bpsz;Uid=sqladmin;Pwd=Password2024;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;";
 
 $conn = odbc_connect($connectionString, '', '');
 if ($conn) {
